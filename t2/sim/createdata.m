56db26c542a3b95537f457691abc04383b5a6d9b
JckeysT <joao.r.chaves@tecnico.ulisboa.pt>
2021-04-02 15:06:54 -0700

    J-Ex4 grafico simulação

--- t2/sim/createdata.m
+++ t2/sim/createdata.m
@@ -35,3 +35,8 @@ fclose (file3);
 file4 = fopen("dataNgspice3.txt","w");
 fprintf(file4,"* supply voltage\n\nVs 1 0 0\n\n* Resistances\n\nR1 2 1 %.11fk\nR2 3 2 %.11fk\nR3 2 5 %.11fk\nR4 0 5 %.11fk\nR5 5 6 %.11fk\nR6 7 0aux %.11fk\nR7 8 7 %.11fk\n\n*Linearly dependent sources\n\nGb 6 3 (2,5) %.11fm\nHc 5 8 vaux %.11fk\n\nvaux 0 0aux DC 0\n\n*Capacitor\n\nc1 6 8 %.11fuF\n\n.ic v(6)=%.11f v(8)=0.0\n\n", A([1]), A([2]), A([3]), A([4]), A([5]), A([6]), A([7]), A([10]), A([11]), A([9]),B([1])-B([2]));
 fclose (file4);
+
+
+file5 = fopen("dataNgspice4.txt","w");
+fprintf(file5,"* supply voltage\n\nVs 1 0 %.11f ac 1.0 sin(0 1 1k)\n\n* Resistances\n\nR1 2 1 %.11fk\nR2 3 2 %.11fk\nR3 2 5 %.11fk\nR4 0 5 %.11fk\nR5 5 6 %.11fk\nR6 7 0aux %.11fk\nR7 8 7 %.11fk\n\n*Linearly dependent sources\n\nGb 6 3 (2,5) %.11fm\nHc 5 8 vaux %.11fk\n\nvaux 0 0aux DC 0\n\n*Capacitor\n\nc1 6 8 %.11fuF\n\n.ic v(6)=%.11f v(8)=0.0\n\n", A([8]), A([1]), A([2]), A([3]), A([4]), A([5]), A([6]), A([7]), A([10]), A([11]), A([9]),B([1])-B([2]));
+fclose (file5);
